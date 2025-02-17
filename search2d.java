a566531c241eb133aa687b7c4b691b90c1343487
Radhika1324 <radhikamishra2413@gmail.com>
2025-02-17 19:30:10 +0530

    Create search2d.java

---
+++ search2d.java
@@ -0,0 +1,14 @@
+class search2d {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        int i,j;
+        for (i=0;i<matrix.length;i++)
+        {
+            for(j=0;j<matrix[0].length;j++)
+            {
+                if(matrix[i][j]==target)
+                {
+                    return true;
+                }
+            }}
+            return false;
+}   }    
